9733b48aa2cb7fe8197d816ff6b2007957254a57
jchediack <jorge.logan@gmail.com>
2024-06-16 22:50:07 -0300

    Projeto Spring Data JPA 2024 - Completo

---
+++ src/main/java/com/bookstore/jpa/services/Util.java
@@ -0,0 +1,14 @@
+package com.bookstore.jpa.services;
+
+public class Util {
+    /*
+INSERT INTO TB_PUBLISHER VALUES (gen_random_uuid(), 'Alta Books');
+INSERT INTO TB_PUBLISHER VALUES (gen_random_uuid(), 'Pearson');
+
+INSERT INTO TB_AUTHOR VALUES (gen_random_uuid(), 'Erik Evans');
+INSERT INTO TB_AUTHOR VALUES (gen_random_uuid(), 'Michael Jame Fox');
+INSERT INTO TB_AUTHOR VALUES (gen_random_uuid(), 'Jeff Silva');
+     */
+
+
+}
